6dc492f589c25ffb12630758602472fe217ef4ea
marcdledda <marcdl636@gmail.com>
2018-01-23 14:28:21 -0600

    edited products.js

--- js/products.js
+++ js/products.js
@@ -1,20 +1,20 @@
 console.log("JS TEST");
 
 var products = [
-    {image: "images/Tech-Fill.png", name: "Product-1"},
-    {image: "images/Tech-Fill.png", name: "Product-2"},
-    {image: "images/Tech-Fill.png", name: "Product-3"},
-    {image: "images/Tech-Fill.png", name: "Product-4"},
-    {image: "images/Tech-Fill.png", name: "Product-5"},
-    {image: "images/Tech-Fill.png", name: "Product-6"}
+    {number: "1", image: "images/Tech-Fill.png", name: "Product-1"},
+    {number: "2", image: "images/Tech-Fill.png", name: "Product-2"},
+    {number: "3", image: "images/Tech-Fill.png", name: "Product-3"},
+    {number: "4", image: "images/Tech-Fill.png", name: "Product-4"},
+    {number: "5", image: "images/Tech-Fill.png", name: "Product-5"},
+    {number: "6", image: "images/Tech-Fill.png", name: "Product-6"}
 ];
 
 var productsInput = "";
 for (i = 0; i < products.length; i++) {
-    productsInput += `<div>`;
+    productsInput += `<div class="product-${products[i].number}">`;
     productsInput += `<img src="${products[i].image}"/>`;
-    productsInput += `<h2>${products[i].name}</h2>`;
-    productsInput += `<a href="#">Learn More</a>`;
+    productsInput += `<h2 class="product-name">${products[i].name}</h2>`;
+    productsInput += `<div class="learn-more">Learn More</div>`;
     productsInput += `</div>`;
 }
 document.getElementById("cards").innerHTML = productsInput;
